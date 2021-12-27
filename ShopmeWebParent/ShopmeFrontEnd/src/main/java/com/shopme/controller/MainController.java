e9894460c199ceb3f7903ba4639a90738ef38836
ngthotuan <ngthotuan@gmail.com>
2021-12-27 20:52:19 +0700

    013 - 127 Code homepage for shopping application

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/controller/MainController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.shopme.shopmefrontend.controller;
+package com.shopme.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
