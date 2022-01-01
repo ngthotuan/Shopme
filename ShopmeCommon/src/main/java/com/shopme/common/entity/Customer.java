7f46c1c0dbf2c13ce845f7a2466b5980dfeb8819
ngthotuan <ngthotuan@gmail.com>
2022-01-01 12:27:32 +0700

    015 - 148 code repository layer for Customer

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Customer.java
@@ -0,0 +1,64 @@
+package com.shopme.common.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
+@ToString
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 64, nullable = false)
+    private String password;
+
+    @Column(length = 45, nullable = false)
+    private String firstName;
+
+    @Column(length = 45, nullable = false)
+    private String lastName;
+
+    @Column(length = 15, nullable = false)
+    private String phoneNumber;
+
+    @Column(length = 64, nullable = false)
+    private String addressLine1;
+
+    @Column(length = 64)
+    private String addressLine2;
+
+    @Column(length = 64, nullable = false)
+    private String city;
+
+    @Column(length = 45, nullable = false)
+    private String state;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    @Column(length = 10, nullable = false)
+    private String postalCode;
+
+    private Date createdTime;
+
+    private boolean enabled = false;
+
+    @Column(length = 64)
+    private String verificationCode;
+
+}
+
---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.shopme.customer;
+
+import com.shopme.common.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String email);
+
+    Customer findByVerificationCode(String verificationCode);
+
+    @Modifying
+    @Query("UPDATE Customer c SET c.enabled = TRUE WHERE c.id = ?1")
+    void enable(Long id);
+}
