5720ad6e25737b3ee217eef24cbf5c789b88f0fc
ngthotuan <ngthotuan@gmail.com>
2022-01-11 11:00:43 +0700

    016 - 162 code customer login & logout

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/MainController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/MainController.java
@@ -2,6 +2,9 @@ package com.shopme;
 
 import com.shopme.category.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,5 +19,14 @@ public class MainController {
     public String viewHomePage(Model model) {
         model.addAttribute("categories", categoryService.listNoChildrenCategories());
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "login";
+        }
+        return "redirect:/";
     }
 }
--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/WebSecurityConfig.java
@@ -1,21 +1,39 @@
 package com.shopme.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomerUserDetailsService();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
     }
 
     @Override
@@ -23,7 +41,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers("/customer/**").authenticated()
+                .anyRequest().permitAll()
+                .and().formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .and().rememberMe()
+                .key("remember-me-key")
+                .tokenValiditySeconds(60 * 60 * 24 * 7)
+        ;
 
     }
 
